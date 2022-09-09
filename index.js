be93c8b3c0683a4b06670473dde65d5887ce2054
Deimantas Tumas <deimantas.tumas@adform.com>
2022-09-09 23:39:36 +0300

    Manage dune transmission

--- index.js
+++ index.js
@@ -8,13 +8,31 @@ const io = require('socket.io')(server, {
     }
   });
 
+let dunes = {};
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', (socket) => {
-    console.log(socket);
+app.get('/delete-dunes', (_, res) => {
+  dunes = {};
+  socket.to("summary").emit('all_dunes', {'dunes': dunes});
+  res.send('OK');
 });
+
+io.on('connection', function(socket){
+  socket.on('join_room', (data) => {
+    console.log(`Someone joined the room ${data.room}`)
+    socket.join(data.room);
+  })
+  socket.on('get_dunes', function(data) {
+    socket.emit('all_dunes', {'dunes': dunes});
+  });
+  socket.on('write_dunes', function(data) {
+    dunes[data.clientId] = data.dunes;
+    socket.to("summary").emit('all_dunes', {'dunes': dunes});
+  })
+ });
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
